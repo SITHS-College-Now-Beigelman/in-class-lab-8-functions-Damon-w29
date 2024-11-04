f5df2a3fe68480525df02266da97e5708ad52765
Damon-w29 <Damonw18@nycstudents.net>
2024-11-04 16:20:08 -0500

    baselines

---
+++ Wu_Damon_Lab_8_pt2.cpp
@@ -0,0 +1,47 @@
+// Damon Wu 
+// 11/4/2024
+// Lecture - 8 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// part 1 
+    double math(double x)
+{
+    double squareroot;
+    squareroot = sqrt(x);
+
+    double power;
+    power = (x, 4);
+
+    double floors;
+    floors = floor(x);
+}
+int main(){
+    double number;
+    cout <<"Input a number. \n";
+    cin >> number; 
+}
+// part 2 
+int doubleMultiply(int a, int b)
+{
+    int product;
+    product = a *b * 2;
+    return product;
+}
+int main(){
+
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    cout << "twice the product of the number is: " 
+            << doubleMultiply(num1, num2) << endl;
+    system("PAUSE");
+    return 0;
+    /*
+    Enter two integers: 10 11
+    twice the product of the number is: 220
+    Press any key to continue . . .
+    */
+}
